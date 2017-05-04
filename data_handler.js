d482b32c40115ff7aa1660e9a5bda44152c3cc31
Vitaly Kramskikh <vkramskikh@gmail.com>
2017-05-05 02:41:06 +0400

    Send data to client right after it's been read

--- data_handler.js
+++ data_handler.js
@@ -3,18 +3,26 @@ import path from 'path';
 
 export default function dataHander(dataDir) {
   return function(req, res) {
-    fs.readdir(dataDir, (err, files) => {
+    fs.readdir(dataDir, (err, fileNames) => {
       if (!err) {
-        const responseBody = '{' + files.reduce((result, file) => {
-          const matchData = file.match(/^(.*?)\.json$/);
+        res.set('Content-Type', 'application/json');
+        const dataFiles = fileNames.reduce((result, fileName) => {
+          const matchData = fileName.match(/^(.*?)\.json$/);
           if (matchData) {
-            const key = matchData[1];
-            const fileContent = fs.readFileSync(path.join(dataDir, file));
-            result.push(JSON.stringify(key) + ':' + fileContent);
+            result.push({
+              fileName: path.join(dataDir, fileName),
+              key: matchData[1]
+            });
           }
           return result;
-        }, []).join(',') + '}';
-        res.set('Content-Type', 'application/json').send(responseBody);
+        }, []);
+        res.write('{');
+        res.write(dataFiles.map(({fileName, key}) => {
+          const fileContent = fs.readFileSync(fileName);
+          return JSON.stringify(key) + ':' + fileContent;
+        }, []).join(','));
+        res.write('}');
+        res.end();
       } else {
         res.status(500).send('Failed to read data directory');
       }
